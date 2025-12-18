d1b1d217f19b71bbbd0cd560939c07ee796c3bec
Natalie <nataliewoerle@hotmail.de>
2025-12-18 18:05:59 +0100

    Click dot to see / set data, drag dot

--- components/Dot.js
+++ components/Dot.js
@@ -1,30 +1,40 @@
-class Dot extends HTMLElement {
-    constructor() {
-        super();
-        // this.attachShadow({mode: "open"});
+export class Dot {
+    constructor(x, y, id, name) {
+        this.x = x;
+        this.y = y;
+        this.id = id || null;
+        this.name = name || '';
+        this.connections = [];
+
+        this.element = this.createDOMElement();
+        this.element.dotInstance = this;
     }
 
-    connectedCallback() {
-        this.innerHTML = `
-        <link rel="stylesheet" href="/components/Dot.css" />
-
-        <div id="circle">
-        </div>
-        `;
-    }
-
-    setPosition(x, y) {
+    updatePosition(x, y) {
+        this.x = x;
+        this.y  = y;
         const size = parseFloat(
             getComputedStyle(document.documentElement)
                 .getPropertyValue('--dot-size')
         );
-        
 
-        this.style.position = 'absolute';
-        this.style.left = `${x - size / 2}px`;
-        this.style.top  = `${y - size / 2}px`;
+        this.element.style.left = `${x - size / 2}px`;
+        this.element.style.top  = `${y - size / 2}px`;
     }
 
-}
+    createDOMElement() {
+        const new_dot = document.createElement('div');
+        new_dot.className = 'stationDot';
 
-customElements.define('station-dot', Dot);
+        const size = parseFloat(
+            getComputedStyle(document.documentElement)
+                .getPropertyValue('--dot-size')
+        );
+
+        new_dot.style.position = 'absolute';
+        new_dot.style.left = `${this.x - size / 2}px`;
+        new_dot.style.top  = `${this.y - size / 2}px`;
+
+        return new_dot;
+    }
+}
--- main.js
+++ main.js
@@ -1,11 +1,68 @@
-import './components/Dot.js';
+import {Dot} from './components/Dot.js'
 
-let outer = document.getElementById('outer');
+let canvas = document.getElementById('outer');
+let draggedDot;
 
-outer.addEventListener('click', e => {
-    const dot = document.createElement('station-dot');
-    dot.setPosition(e.pageX, e.pageY);
-    outer.appendChild(dot);
-    console.log("H");
-    
+function addDotListener(type, target, callback) {
+    target.addEventListener(type, e => {
+        if (e.target.classList.contains('stationDot')) {
+            const dot = e.target.dotInstance;
+            callback(e, dot);
+        }
+    });
+}
+
+canvas.addEventListener('click', e => {
+    if (e.target == canvas) {
+        const dot = new Dot(e.pageX, e.pageY);
+        canvas.appendChild(dot.element);
+        console.log("Created dot with coordinates", e.pageX, e.pageY);
+    }
 });
+
+// Single click and double click dots
+
+addDotListener('click', canvas, (e, dot) => {
+    // Single click to see data
+    if (e.detail == 1) {
+        console.log("Clicked dot", dot.x, dot.y, dot.name);
+    }
+
+    // Double click to change data
+    if (e.detail == 2) {
+        const curr_name = dot.name;
+        const new_name = prompt("Station name:");
+        dot.name = (new_name) ? new_name : curr_name;
+        console.log("New name:", dot.name)
+    }
+})
+
+// Start drag
+
+addDotListener('mousedown', canvas, (e,dot) => {;
+    dot.element.classList.add('dragging');
+    draggedDot = dot;
+})
+
+// During drag
+
+canvas.addEventListener('mousemove', e => {
+    const size = parseFloat(
+        getComputedStyle(document.documentElement)
+            .getPropertyValue('--dot-size')
+    );
+
+    if(draggedDot) {
+        let new_x = e.pageX;
+        let new_y = e.pageY;
+        
+        draggedDot.updatePosition(new_x, new_y);
+    }
+})
+
+// End drag
+
+addDotListener('mouseup', canvas, e => {
+    draggedDot.element.classList.remove('dragging');
+    draggedDot = null;
+})
